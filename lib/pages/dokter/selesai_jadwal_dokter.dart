bc301b810f24cfc4c563ac280bf7d6232af244da
rizwaradnan03 <rizwar.adnanbyantara@gmail.com>
2024-12-17 02:37:00 +0700

    finishin selesai jadwal

--- lib/pages/dokter/selesai_jadwal_dokter.dart
+++ lib/pages/dokter/selesai_jadwal_dokter.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:uap_pbo/database/data.dart';
+import 'package:uap_pbo/option/DokterOption.dart';
 import 'package:uap_pbo/option/PenjadwalanOption.dart';
+import 'package:uap_pbo/pages/dokter/main_dokter.dart';
 
 class SelesaiJadwalDokter extends StatefulWidget {
   const SelesaiJadwalDokter({super.key});
@@ -13,24 +15,61 @@ class _SelesaiJadwalDokterState extends State<SelesaiJadwalDokter> {
   final TextEditingController _searchByNamaController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
 
+  DokterOption? dokter;
   bool isFound = false;
   PenjadwalanOption? _selectedListPraktek;
- List<PenjadwalanOption?> penjadwalan = [];
+  List<PenjadwalanOption?> penjadwalan = [];
 
   void _searchByNama() {
-    bool isDokterFound = false;
-
     for (int i = 0; i < dataDokter.length; i++) {
       if (_searchByNamaController.text == dataDokter[i].nama) {
-        isFound = true;
-        isDokterFound = true;
-        penjadwalan.addAll(dataDokter[i].listPraktek as List<PenjadwalanOption?>);
+        setState(() {
+          isFound = true;
+        });
+        dokter = dataDokter[i];
+        penjadwalan.addAll(dataDokter[i].listPraktek);
         break;
       }
     }
+  }
 
-    if (isDokterFound == true) {
-      debugPrint("Penjadwalan Dokter nyaah ${penjadwalan}");
+  void _submit() {
+    bool isPenjadwalanFound = false;
+    bool isRemoveSuccess = false;
+    for (int i = 0; i < dataPenjadwalan.length; i++) {
+      if (_selectedListPraktek?.hari == dataPenjadwalan[i].hari &&
+          _selectedListPraktek?.waktuMulai == dataPenjadwalan[i].waktuMulai &&
+          _selectedListPraktek?.waktuSelesai ==
+              dataPenjadwalan[i].waktuSelesai) {
+        dataPenjadwalan.removeAt(i);
+        isPenjadwalanFound = true;
+      }
+    }
+
+    if (isPenjadwalanFound == true) {
+      if (dokter != null) {
+        for (int i = 0; i < dokter!.listPraktek.length; i++) {
+          if (dokter!.listPraktek[i].hari == _selectedListPraktek?.hari &&
+              dokter!.listPraktek[i].waktuMulai ==
+                  _selectedListPraktek?.waktuMulai &&
+              dokter!.listPraktek[i].waktuSelesai ==
+                  _selectedListPraktek?.waktuSelesai) {
+            dokter!.listPraktek.removeAt(i);
+            isRemoveSuccess = true;
+            break;
+          }
+        }
+      }
+    }
+
+    if (isRemoveSuccess == true) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("Berhasil Menyelesaikan Jadwal Dokter!")));
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => MainDokter()));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Gagal Menyelesaikan Jadwal!")));
     }
   }
 
@@ -76,26 +115,35 @@ class _SelesaiJadwalDokterState extends State<SelesaiJadwalDokter> {
                         onPressed: _searchByNama,
                         child: const Text('Cari'),
                       ),
-                      isFound == true? Column(
-                        children: [
-                          DropdownButton<PenjadwalanOption> (
-                            value: _selectedListPraktek,
-                            hint: const Text("Pilih Jawdwal untuk Dihapus"),
-                            items: penjadwalan.where(( item)=>item !=null).map((item){
-                              return DropdownMenuItem<PenjadwalanOption>(
-                                value: item,
-                                child: Text(
-                                      "(${item?.hari}) ${item?.waktuMulai} - ${item?.waktuSelesai}"),
-                              );
-                            }).toList(),
-                            onChanged: (PenjadwalanOption? newValue){
-                              setState(() {
-                                _selectedListPraktek = newValue;
-                              });
-                            },
-                          ),ElevatedButton(onPressed: _submit,)
-                        ],
-                      ):SizedBox.shrink(),
+                      isFound == true
+                          ? Column(
+                              children: [
+                                DropdownButton<PenjadwalanOption>(
+                                  value: _selectedListPraktek,
+                                  hint:
+                                      const Text("Pilih Jawdwal untuk Dihapus"),
+                                  items: penjadwalan
+                                      .where((item) => item != null)
+                                      .map((item) {
+                                    return DropdownMenuItem<PenjadwalanOption>(
+                                      value: item,
+                                      child: Text(
+                                          "(${item?.hari}) ${item?.waktuMulai} - ${item?.waktuSelesai}"),
+                                    );
+                                  }).toList(),
+                                  onChanged: (PenjadwalanOption? newValue) {
+                                    setState(() {
+                                      _selectedListPraktek = newValue;
+                                    });
+                                  },
+                                ),
+                                ElevatedButton(
+                                  onPressed: _submit,
+                                  child: const Text("Submit"),
+                                )
+                              ],
+                            )
+                          : SizedBox.shrink(),
                     ],
                   ),
                 ),
